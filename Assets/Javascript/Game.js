ecf0e4a1689ffeb2cf1c15ef5edc8ba0bd67007a
unknown <mauro.hdz99@gmail.com>
2019-03-20 15:24:42 -0500

    built html, connected javascript

---
+++ Assets/Javascript/Game.js
@@ -0,0 +1,29 @@
+// The pc chooses a random score between 20 and 120
+var pcscore = Math.floor((Math.random() * 100) + 20)
+console.log;
+var scoretracker = 0;
+
+var 
+
+
+
+
+
+
+// 4 different variables. One for each button. Each button gets a random value every round.
+
+// A score that keeps track(adds) up the values of each button. 
+
+// Something that resets the game if the usr score goes over the pc score.
+
+
+// functions
+("#pcnumber").textcontent
+
+$('#cake').on('click', function() {
+
+
+ 
+
+
+})
